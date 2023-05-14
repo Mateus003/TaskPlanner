6c4c802d81e2f5b4e401aa36b8f8c6d4e77ab3f6
Mateus da Silva <mso3@cin.ufpe.br>
2023-05-14 19:51:04 -0300

    add Firebase realtime Database

--- app/src/main/java/br/com/example/taskplanner/ui/FormTaskFragment.kt
+++ app/src/main/java/br/com/example/taskplanner/ui/FormTaskFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.view.isVisible
 import br.com.example.taskplanner.R
 import br.com.example.taskplanner.databinding.FragmentFormTaskBinding
 import br.com.example.taskplanner.util.initToolbar
 import br.com.example.taskplanner.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class FormTaskFragment : Fragment() {
 
     private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
+    private lateinit var task: br.com.example.taskplanner.data.model.Task
+    private var newTask: Boolean = true
+    private lateinit var status: br.com.example.taskplanner.data.model.Status
+    private lateinit var reference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,27 +37,58 @@ class FormTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
-
+        reference = Firebase.database.reference
+        auth = Firebase.auth
+        status = br.com.example.taskplanner.data.model.Status.TODO
         initListeners()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun initListeners() {
         binding.btnSave.setOnClickListener { validateData() }
+
+        binding.rgStatus.setOnCheckedChangeListener {_, id ->
+            status = when(id){
+                R.id.rbTodo-> {br.com.example.taskplanner.data.model.Status.TODO}
+                R.id.rbDoing-> {br.com.example.taskplanner.data.model.Status.DOING}
+                else->{br.com.example.taskplanner.data.model.Status.DONE}
+            }
+
+        }
     }
 
     private fun validateData() {
         val description = binding.edtDescription.text.toString().trim()
 
         if (description.isNotEmpty()) {
-            Toast.makeText(requireContext(), "Tudo Certo.", Toast.LENGTH_SHORT).show()
+            binding.progressBar.isVisible = true
+
+            if (newTask) task = br.com.example.taskplanner.data.model.Task()
+            task.description = description
+            task.status = status
+            task.id = reference.database.reference.push().key ?:""
+
+            saveTask()
+
         } else {
             showBottomSheet(message = getString(R.string.description_empty_form_task_fragment))
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+    private fun saveTask(){
+        reference
+            .child("tasks")
+            .child(auth.currentUser?.uid ?: "")
+            .child(task.id)
+            .setValue(task)
+
+
+
+
     }
 
 }
