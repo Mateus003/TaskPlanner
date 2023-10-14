22fa4a4bdb0e27368326a5088680b04e78a2a0aa
Mateus da Silva <mso3@cin.ufpe.br>
2023-10-14 07:13:06 -0300

    created Done Fragment

--- app/src/main/java/com/example/taskplanner/View/fragments/DoneFragment.kt
+++ app/src/main/java/com/example/taskplanner/View/fragments/DoneFragment.kt
@@ -6,29 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.taskplanner.R
-import com.example.taskplanner.databinding.FragmentRegisterBinding
+import com.example.taskplanner.databinding.FragmentDoneBinding
 
-class RegisterFragment : Fragment() {
-    private var _binding: FragmentRegisterBinding? = null
-    private val binding get() = _binding!!
-
+class DoneFragment : Fragment() {
+    private lateinit var binding: FragmentDoneBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        binding = FragmentDoneBinding.inflate(inflater, container, false)
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
 
 }
