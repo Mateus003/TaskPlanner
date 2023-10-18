96a4fa6c0b8638fe3c5505e914e227516db49dea
Mateus da Silva <mso3@cin.ufpe.br>
2023-10-18 18:55:27 -0300

    modify type textCategory

--- app/src/main/java/com/example/taskplanner/model/TaskCategory.kt
+++ app/src/main/java/com/example/taskplanner/model/TaskCategory.kt
@@ -4,6 +4,6 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 
 data class TaskCategory(
-     val textCategory: TextView,
+     val textCategory: String,
      val containerTextCategory: CardView
 )
