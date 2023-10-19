0ba2e88cfb6870325c712c3c55d29653760642db
Mateus da Silva <mso3@cin.ufpe.br>
2023-10-19 20:59:10 -0300

    Created a BaseFragment class for hiding the keyboard.

---
+++ app/src/main/java/com/example/taskplanner/View/fragments/BaseFragment.kt
@@ -0,0 +1,18 @@
+package com.example.taskplanner.View.fragments
+
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+
+open class BaseFragment: Fragment() {
+
+    //Esconder teclado do dispostivo
+    fun hideKeyboard(){
+        val view = activity?.currentFocus
+        if (view!=null){
+            val inm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+    }
+}
